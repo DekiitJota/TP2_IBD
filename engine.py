a1b8e224b374fa35717bc56a77d6232ad2a28537
DekiitJota <41349933+DekiitJota@users.noreply.github.com>
2021-08-20 21:32:32 -0300

    Add files via upload
    Development update on the SQL translator engine for "preco_medicamentos"

--- engine.py
+++ engine.py
@@ -9,67 +9,92 @@ cur = conn.cursor()
 conn.executescript('''
     PRAGMA foreign_keys=OFF;
     BEGIN TRANSACTION;
-    CREATE TABLE PRODUTO (
+    CREATE TABLE IF NOT EXISTS PRODUTO (
         ID_PRODUTO INTEGER PRIMARY KEY, 
         NOME TEXT,
         CLASSE TEXT
-        ID_REG INTEGER REFERENCES REGISTRO (ID_REG)
+        ID_REG INTEGER REFERENCES REGISTRO (ID_REG),
+        ID_SUBSTANCIA INTEGER REFERENCES SUBSTANCIA (ID_SUBSTANCIA),
+        ID_APRESENTACAO INTEGER REFERENCES APRESENTACAO (ID_APRESENTACAO),
+        ID_TIPO INTEGER REFERENCES TIPO (ID_TIPO)
     );
-    CREATE TABLE REGISTRO (
+    CREATE TABLE IF NOT EXISTS REGISTRO (
         ID_REG INTEGER PRIMARY KEY,
         EAN1 VARCHAR,
         PRECO_MAXIMO NUMERIC,
-        COD_REGISTRO INTEGER
+        COD_REGISTRO INTEGER,
+        ID_LAB REFERENCE LABORATORIO (ID_LABORATORIO)
     );
-    CREATE TABLE LABORATORIO (
+    CREATE TABLE IF NOT EXISTS LABORATORIO (
         ID_LAB INTEGER PRIMARY KEY,
         NOME_LAB VARCHAR,
         CNPJ VARCHAR,
         ID_REG INTEGER REFERENCES REGISTRO (ID_REG)
     );
-    CREATE TABLE SUBSTANCIA (
+    CREATE TABLE IF NOT EXISTS SUBSTANCIA (
         ID_SUBSTANCIA TEXT,
         NOME TEXT			
     ); 
-    CREATE TABLE APRESENTACAO (
+    CREATE TABLE IF NOT EXISTS APRESENTACAO (
         ID_APRESENTACAO INTEGER PRIMARY KEY,
         DESCRICAO TEXT,
         COD_GGREM INTEGER
     );
-    CREATE TABLE APRESENTACAO (
-        ID_TIPO INTEGER,
-        NOME TEXT,
-        PRIMARY KEY(ID_TIPO)
+    CREATE TABLE IF NOT EXISTS TIPO (
+        ID_TIPO INTEGER PRIMARY KEY,
+        NOME TEXT
     );
+    COMMIT;
 ''')
 
 with open('/tmp/TA_PRECO_MEDICAMENTO.csv', 'r') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     for row in csv_reader:
-        _id=row[0]
-        dataNotificacao=row[1]
-        cnes=row[2]
-        ocupacaoSuspeitoCli=float(row[3])
-        ocupacaoSuspeitoUti=float(row[4])
-        ocupacaoConfirmadoCli=float(row[5])
-        ocupacaoConfirmadoUti=float(row[6])
-        saidaSuspeitaObitos=float(row[7])
-        saidaSuspeitaAltas=float(row[8])
-        saidaConfirmadaObitos=float(row[9])
-        saidaConfirmadaAltas=float(row[10])
+        substancia=row[0]
+        cnpj=row[1]
+        codigo_ggrem=row[2]
+        registro=row[3]
+        ean1=row[4]
+        ean2=row[5]
+        ean2=row[6]
+        produto_apresentacao=row[7]
+        classe=row[8]
+        tipo=row[9]
+        regime_preco=row[10]
         origem=row[11]
-        _p_usuario=row[12]
-        estadoNotificacao=row[13]
-        municipioNotificacao=row[14]
-        estado=row[15]
-        municipio=row[16]
-        excluido=row[17]
-        validado=row[18]
-        _created_at=row[19]
-        _updated_at=row[20]
-        
-        cur.executescript('''
-        INSERT INTO registro_ocupacao(_id,dataNotificacao,cnes,ocupacaoSuspeitoCli,ocupacaoSuspeitoUti,ocupacaoConfirmadoCli,ocupacaoConfirmadoUti,saidaSuspeitaObitos,saidaSuspeitaAltas,saidaConfirmadaObitos,saidaConfirmadaAltas,origem,_p_usuario,estadoNotificacao,municipioNotificacao,estado,municipio,excluido,validado,_created_at,_updated_at)
+        preco_fab=row[12] #preco de fabrica s/ impostos
+        #preço de fabrica @ x% = Xpc
+        preco_fab_0pc=row[13] 
+        preco_fab_12pc=row[14]
+        preco_fab_17pc=row[15]
+        preco_fab_17pc_alc=row[16]
+        preco_fab_17_5pc=row[17]
+        preco_fab_17_5pc_alc=row[18]
+        preco_fab_18pc=row[19]
+        preco_fab_18pc_alc=row[20]
+        preco_fab_20pc=row[21]
+        #preço de mercado @ x% = Xpc
+        preco_mc_0pc=row[22]
+        preco_mc_12pc=row[23]
+        preco_mc_17pc=row[24]
+        preco_mc_17pc_alc=row[25]
+        preco_mc_17_5pc=row[26]
+        preco_mc_17_5pc_alc=row[27]
+        preco_mc_18pc=row[28]
+        preco_mc_18pc_alc=row[29]
+        preco_mc_20pc=row[30]
+        restricao_hosp=row[31]
+        cap=row[32]
+        confaz=row[33]
+        icms=row[34]
+        analise_recursal=row[35]
+        cr_tributario=row[36]
+        comercializacao=row[37]
+        tarja=row[38]
+
+cur.executescript('''
+    INSERT INTO registro_ocupacao(_id,dataNotificacao,cnes,ocupacaoSuspeitoCli,ocupacaoSuspeitoUti,ocupacaoConfirmadoCli,ocupacaoConfirmadoUti,saidaSuspeitaObitos,saidaSuspeitaAltas,saidaConfirmadaObitos,saidaConfirmadaAltas,origem,_p_usuario,estadoNotificacao,municipioNotificacao,estado,municipio,excluido,validado,_created_at,_updated_at)
         VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)
-        '''), (_id,dataNotificacao,cnes,ocupacaoSuspeitoCli,ocupacaoSuspeitoUti,ocupacaoConfirmadoCli,ocupacaoConfirmadoUti,saidaSuspeitaObitos,saidaSuspeitaAltas,saidaConfirmadaObitos,saidaConfirmadaAltas,origem,_p_usuario,estadoNotificacao,municipioNotificacao,estado,municipio,excluido,validado,_created_at,_updated_at)
-        conn.commit()
+'''), (_id,dataNotificacao,cnes,ocupacaoSuspeitoCli,ocupacaoSuspeitoUti,ocupacaoConfirmadoCli,ocupacaoConfirmadoUti,saidaSuspeitaObitos,saidaSuspeitaAltas,saidaConfirmadaObitos,saidaConfirmadaAltas,origem,_p_usuario,estadoNotificacao,municipioNotificacao,estado,municipio,excluido,validado,_created_at,_updated_at)
+
+conn.commit()
