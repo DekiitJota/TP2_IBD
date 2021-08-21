63d986379da52719ffb9f11c20814c9fb6259dc4
DekiitJota <41349933+DekiitJota@users.noreply.github.com>
2021-08-21 01:10:04 -0300

    Update engine.py

--- engine.py
+++ engine.py
@@ -12,7 +12,8 @@ conn.executescript('''
     CREATE TABLE IF NOT EXISTS PRODUTO (
         ID_PRODUTO INTEGER PRIMARY KEY, 
         NOME TEXT,
-        CLASSE TEXT
+        CLASSE TEXT,
+        TARJA TEXT,
         ID_REG INTEGER REFERENCES REGISTRO (ID_REG),
         ID_SUBSTANCIA INTEGER REFERENCES SUBSTANCIA (ID_SUBSTANCIA),
         ID_APRESENTACAO INTEGER REFERENCES APRESENTACAO (ID_APRESENTACAO),
@@ -22,11 +23,10 @@ conn.executescript('''
         ID_REG INTEGER PRIMARY KEY,
         EAN1 TEXT,
         PRECO_MAXIMO NUMERIC,
-        COD_REGISTRO INTEGER,
-        ID_LABORATORIO REFERENCE LABORATORIO (ID_LABORATORIO)
+        COD_REGISTRO INTEGER
     );
     CREATE TABLE IF NOT EXISTS LABORATORIO (
-        ID_LABORATORIO INTEGER PRIMARY KEY,
+        ID_LAB INTEGER PRIMARY KEY,
         NOME_LAB TEXT,
         CNPJ TEXT,
         ID_REG INTEGER REFERENCES REGISTRO (ID_REG)
@@ -42,7 +42,7 @@ conn.executescript('''
     );
     CREATE TABLE IF NOT EXISTS TIPO (
         ID_TIPO INTEGER PRIMARY KEY,
-        NOME_TIPO TEXT
+        STATUS TEXT
     );
     COMMIT;
 ''')
@@ -61,54 +61,52 @@ with open('/tmp/TA_PRECO_MEDICAMENTO.csv', 'r') as csv_file:
         produto=row[8]
         descricao=row[9]
         classe=row[10]
-        tipo=row[11]
+        status=row[11]
         regime_preco=row[12]
-        origem=row[13]
-        preco_fab=row[14] #preco de fabrica s/ impostos
+        preco_fab=row[13] #preco de fabrica s/ impostos
         #preço de fabrica @ x% = Xpc
-        preco_fab_0pc=row[15] 
-        preco_fab_12pc=row[16]
-        preco_fab_17pc=row[17]
-        preco_fab_17pc_alc=row[18]
-        preco_fab_17_5pc=row[19]
-        preco_fab_17_5pc_alc=row[20]
-        preco_fab_18pc=row[21]
-        preco_fab_18pc_alc=row[22]
-        preco_fab_20pc=row[23]
+        preco_fab_0pc=row[14] 
+        preco_fab_12pc=row[15]
+        preco_fab_17pc=row[16]
+        preco_fab_17pc_alc=row[17]
+        preco_fab_17_5pc=row[18]
+        preco_fab_17_5pc_alc=row[19]
+        preco_fab_18pc=row[20]
+        preco_fab_18pc_alc=row[21]
+        preco_fab_20pc=row[22]
         #preço de mercado @ x% = Xpc
-        preco_mc_0pc=row[24]
-        preco_mc_12pc=row[25]
-        preco_mc_17pc=row[26]
-        preco_mc_17pc_alc=row[27]
-        preco_mc_17_5pc=row[28]
-        preco_mc_17_5pc_alc=row[29]
-        preco_mc_18pc=row[30]
-        preco_mc_18pc_alc=row[31]
-        preco_mc_20pc=row[32]
-        restricao_hosp=row[33]
-        cap=row[34]
-        confaz=row[35]
-        icms=row[36]
-        analise_recursal=row[37]
-        cr_tributario=row[38]
-        comercializacao=row[39]
-        tarja=row[40]
+        preco_mc_0pc=row[23]
+        preco_mc_12pc=row[24]
+        preco_mc_17pc=row[25]
+        preco_mc_17pc_alc=row[26]
+        preco_mc_17_5pc=row[27]
+        preco_mc_17_5pc_alc=row[28]
+        preco_mc_18pc=row[29]
+        preco_mc_18pc_alc=row[30]
+        preco_mc_20pc=row[31]
+        restricao_hosp=row[32]
+        cap=row[33]
+        confaz=row[34]
+        icms=row[35]
+        analise_recursal=row[36]
+        cr_tributario=row[37]
+        comercializacao=row[38]
+        tarja=row[39]
 
 #TO DO: Implementar identadores para SUBSTANCIA, REGISTRO, LABORATORIO, SUBSTANCIA, APRESENTACAO, TIPO
 
 cur.executescript('''
-    INSERT INTO PRODUTO(ID_PRODUTO,NOME,CLASSE,ID_REG,ID_SUBSTANCIA,ID_APRESENTACAO,ID_TIPO)
-        VALUES (?,?,?,?,?,?,?)
-'''), (id_produto,produto,classe,id_reg,id_substancia,id_apresentacao,id_tipo)
+    INSERT INTO PRODUTO(ID_PRODUTO,NOME,CLASSE,TARJA,ID_REG,ID_SUBSTANCIA,ID_APRESENTACAO,ID_TIPO)
+        VALUES (?,?,?,?,?,?,?,?)
+'''), (id_produto,produto,classe,tarja,id_reg,id_substancia,id_apresentacao,id_tipo)
 
-#TO DO: rever "PRECO_MAXIMO"
 cur.executescript('''
-    INSERT INTO REGISTRO(ID_REG,EAN1,PRECO_MAXIMO,REGISTRO,ID_LABORATORIO) 
+    INSERT INTO REGISTRO(ID_REG,EAN1,PRECO_MAXIMO,REGISTRO,ID_LAB) 
         VALUES (?,?,?,?,?)
 '''), (id_reg,ean1,preco_mc_20pc,registro,id_lab)
 
 cur.executescript('''
-    INSERT INTO LABORATORIO(ID_LABORATORIO,NOME_LAB,CNPJ,ID_REG) 
+    INSERT INTO LABORATORIO(ID_LAB,NOME_LAB,CNPJ,ID_REG) 
         VALUES (?,?,?,?)
 '''), (id_lab,laboratorio,cnpj,id_reg)
 
@@ -122,10 +120,9 @@ cur.executescript('''
         VALUES (?,?,?)
 '''), (id_apresentacao,descricao,codigo_ggrem)
 
-#TIPO = TARJA?
 cur.executescript('''
-    INSERT INTO TARJA(ID_TARJA,NOME_TARJA) 
+    INSERT INTO TIPO(ID_TIPO,STATUS) 
         VALUES (?,?)
-'''), (id_tarja,tarja)
+'''), (id_tarja,status)
 
 conn.commit()
