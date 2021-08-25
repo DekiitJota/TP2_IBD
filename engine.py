742ae65289275e9ca10c82d6550812bac3e078d4
DekiitJota <41349933+DekiitJota@users.noreply.github.com>
2021-08-25 11:33:17 -0300

    Update engine.py
    Refactoring table ids. UUID creation Attempt. Reasoning:
    
    >In SQLite, a column with type INTEGER PRIMARY KEY is an alias for the ROWID (except in WITHOUT ROWID tables) which is always a 64-bit signed integer.
    
    >The parent key is the column or set of columns in the parent table that the foreign key constraint refers to. This is normally, but not always, the primary key of the parent table. The parent key must be a named column or columns in the parent table, NOT THE ROWID.
    
    > AUTOINCREMENT used on primary keys, The parent key is the column or set of columns in the parent table that the foreign key constraint refers to. This is normally, but not always, the primary key of the parent table. The parent key must be a named column or columns in the parent table, not the rowid.
    
    !>The AUTOINCREMENT keyword imposes extra CPU, memory, disk space, and disk I/O overhead and should be avoided if not strictly needed. It is usually not needed.

--- engine.py
+++ engine.py
@@ -1,12 +1,13 @@
 import sqlite3
 import csv
+import uuid
 import pandas as pd
 
 conn = sqlite3.connect('/tmp/preco_medicamentos.sql')
 cur = conn.cursor()
 
 cur.executescript("""
-    PRAGMA foreign_keys=OFF;
+    PRAGMA foreign_keys=ON;
     BEGIN TRANSACTION;
     CREATE TABLE IF NOT EXISTS PRODUTO (
         ID_PRODUTO INTEGER PRIMARY KEY, 
@@ -16,7 +17,7 @@ cur.executescript("""
         ID_TIPO INTEGER,
           FOREIGN KEY (ID_SUBSTANCIA) REFERENCES SUBSTANCIA (ID_SUBSTANCIA),
           FOREIGN KEY (ID_TIPO) REFERENCES TIPO (ID_TIPO)
-    );
+    ) WITHOUT ROWID;
     CREATE TABLE IF NOT EXISTS REGISTRO (
         ID_REG INTEGER PRIMARY KEY,
         EAN1 TEXT,
@@ -27,27 +28,27 @@ cur.executescript("""
         ID_PRODUTO INTEGER,
           FOREIGN KEY (ID_PRODUTO) REFERENCES PRODUTO (ID_PRODUTO),
           FOREIGN KEY (ID_LAB) REFERENCES LABORATORIO (ID_LAB)
-    );
+    ) WITHOUT ROWID;
     CREATE TABLE IF NOT EXISTS LABORATORIO (
         ID_LAB INTEGER PRIMARY KEY,
         NOME_LAB TEXT,
         CNPJ TEXT
-    );
+    ) WITHOUT ROWID;
     CREATE TABLE IF NOT EXISTS SUBSTANCIA (
         ID_SUBSTANCIA INTEGER PRIMARY KEY,
         NOME_SUBS TEXT			
-    ); 
+    ) WITHOUT ROWID; 
     CREATE TABLE IF NOT EXISTS APRESENTACAO (
         ID_APRESENTACAO INTEGER PRIMARY KEY,
         DESCRICAO TEXT,
         COD_GGREM INTEGER,
         ID_PRODUTO INTEGER,
           FOREIGN KEY (ID_PRODUTO) REFERENCES PRODUTO (ID_PRODUTO)
-    );
+    ) WITHOUT ROWID;
     CREATE TABLE IF NOT EXISTS TIPO (
         ID_TIPO INTEGER PRIMARY KEY,
         STATUS TEXT
-    );
+    ) WITHOUT ROWID;
     COMMIT;
 """)
 
@@ -97,19 +98,28 @@ with open('/tmp/test2.csv', 'r') as csv_file:
         cr_tributario=row[37]
         comercializacao=row[38]
         tarja=row[39]
+        
+        id_produto = str(uuid.uuid4().int).replace('-','')
+        id_reg = str(uuid.uuid4().int).replace('-','')
+        id_lab = str(uuid.uuid4().int).replace('-','')
+        id_subs = str(uuid.uuid4().int).replace('-','')
+        id_apresentacao = str(uuid.uuid4().int).replace('-','')
+        id_tipo = str(uuid.uuid4().int).replace('-','')
+
         with conn:
-            cur.execute("INSERT INTO PRODUTO(NOME,CLASSE) VALUES (?,?)", (produto,classe))
-            cur.execute("INSERT INTO REGISTRO(EAN1,TARJA,PRECO_MAXIMO,COD_REGISTRO) VALUES (?,?,?,?)", (ean1,tarja,preco_mc_20pc,registro))
-            cur.execute("INSERT INTO LABORATORIO(NOME_LAB,CNPJ) VALUES (?,?)", (laboratorio,cnpj))
-            cur.execute("INSERT INTO SUBSTANCIA(NOME_SUBS) VALUES (?)", (substancia,))
-            cur.execute("INSERT INTO APRESENTACAO(DESCRICAO,COD_GGREM)VALUES (?,?)", (descricao,codigo_ggrem))
-            cur.execute("INSERT INTO TIPO(STATUS) VALUES (?)", (status,))
+            cur.execute("INSERT INTO PRODUTO(ID_PRODUTO,NOME,CLASSE) VALUES (?,?,?)", (id_produto,produto,classe))
+            cur.execute("INSERT INTO REGISTRO(ID_REG,EAN1,TARJA,PRECO_MAXIMO,COD_REGISTRO) VALUES (?,?,?,?,?)", (id_reg,ean1,tarja,preco_mc_20pc,registro))
+            cur.execute("INSERT INTO LABORATORIO(ID_LAB,NOME_LAB,CNPJ) VALUES (?,?,?)", (id_lab,laboratorio,cnpj))
+            cur.execute("INSERT INTO SUBSTANCIA(ID_SUBSTANCIA,NOME_SUBS) VALUES (?,?)", (id_subs,substancia))
+            cur.execute("INSERT INTO APRESENTACAO(ID_APRESENTACAO,DESCRICAO,COD_GGREM)VALUES (?,?,?)", (id_apresentacao,descricao,codigo_ggrem))
+            cur.execute("INSERT INTO TIPO(ID_TIPO,STATUS) VALUES (?,?)", (id_tipo,status))
         
         #deleta o header do arquivo csv:
         cur.execute("DELETE FROM PRODUTO WHERE (NOME == 'PRODUTO')")
         cur.execute("DELETE FROM REGISTRO WHERE (COD_REGISTRO == 'REGISTRO')")
-        cur.execute("DELETE FROM LABORATORIO WHERE (NOME_LAB == 'LABORATORIO')")
-        cur.execute("DELETE FROM SUBSTANCIA WHERE (NOME_SUBS == 'SUBSTANCIA')")
-        cur.execute("DELETE FROM APRESENTACAO WHERE (DESCRICAO == 'APRESENTACAO')")
+        cur.execute("DELETE FROM LABORATORIO WHERE (NOME_LAB == 'LABORATÓRIO')")
+        cur.execute("DELETE FROM SUBSTANCIA WHERE (NOME_SUBS == 'SUBSTÂNCIA')")
+        cur.execute("DELETE FROM APRESENTACAO WHERE (DESCRICAO == 'APRESENTAÇÃO')")
         cur.execute("DELETE FROM TIPO WHERE (STATUS == 'TIPO DE PRODUTO (STATUS DO PRODUTO)')")
+
 conn.commit()
